1de62750808cc41386debea5b4c25751a8e2e987
Alfredo Navas <elpuas@gmail.com>
2020-11-11 17:35:49 -0600

    adjustments

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,13 +62,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-default-mdx-basic',
-        short_name: 'starter',
+        name: 'el-puas-blog',
+        short_name: 'el-puas',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#000729',
+        theme_color: '#000729',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/skull-icon.svg', // This path is relative to the root of the site.
       },
     },
     {
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+  ],
   theme: {
     container: {
       center: true,
