f066796587b507529dba3933e7d8a25dde92c4a7
Alfredo Navas <elpuas@gmail.com>
2021-04-05 23:28:45 -0600

    fix env netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
           private_key_id: process.env.GATSBY_PRIVATE_KEY_ID,
           private_key: JSON.parse( process.env.GATSBY_PRIVATE_KEY.replace(/(\\r)|(\\n)/g, '\n') ),
           client_email: process.env.GATSBY_CLIENT_EMAIL,
-          client_id: '',
+          client_id: process.env.GATSBY_CLIENT_ID,
           auth_uri: 'https://accounts.google.com/o/oauth2/auth',
           token_uri: 'https://oauth2.googleapis.com/token',
           auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
