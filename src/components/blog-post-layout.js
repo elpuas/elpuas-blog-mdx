cd4fe55bb3d37e1643e562cb0b69b9cb0a35ae88
Alfredo Navas <elpuas@gmail.com>
2021-04-30 13:12:59 -0600

    update layout by lang

--- src/components/blog-post-layout.js
+++ src/components/blog-post-layout.js
@@ -1,34 +1,60 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/tag'
 import Layout from './layout'
+import LayoutEs from './layout.es'
 import Helmet from 'react-helmet'
 import Seo from './seo'
 import { defineCustomElements as deckDeckGoHighlightElement } from '@deckdeckgo/highlight-code/dist/loader';
 
 const BlogPostLayout = ({ children, pageContext }) => {
   deckDeckGoHighlightElement();
-  const { title, author } = pageContext.frontmatter
+  const { title, author, language } = pageContext.frontmatter
 
-  return (
-    <Layout>
-      <Helmet>
-        <title>{title}</title>
-      </Helmet>
-      <Seo
-        title={title}
-        description={title}
-      />
-      <article>
-        <header>
-          <h2>{title}</h2>
-          <div>
-            <p>Author: {author} </p>
-          </div>
-        </header>
-        <MDXProvider>{children}</MDXProvider>
-      </article>
-    </Layout>
-  )
+  if (language && language === 'ES') {
+    return (
+    <LayoutEs>
+        <Helmet>
+          <title>{title}</title>
+        </Helmet>
+        <Seo
+          title={title}
+          description={title}
+        />
+        <article>
+          <header>
+            <h2>{title}</h2>
+            <div>
+              <p>Author: {author} </p>
+            </div>
+          </header>
+          <MDXProvider>{children}</MDXProvider>
+        </article>
+      </LayoutEs>
+    )
+  } else {
+    return (
+      <Layout>
+        <Helmet>
+          <title>{title}</title>
+        </Helmet>
+        <Seo
+          title={title}
+          description={title}
+        />
+        <article>
+          <header>
+            <h2>{title}</h2>
+            <div>
+              <p>Author: {author} </p>
+            </div>
+          </header>
+          <MDXProvider>{children}</MDXProvider>
+        </article>
+      </Layout>
+    )
+  }
+
+
 }
 
 export default BlogPostLayout
